ddeeeaefb62e67f46dfedbab0f5202f081aa988c
quroneko <lycorishub@gmail.com>
2024-11-20 15:00:27 +0900

    chore: ci/cd testing

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -12,6 +12,9 @@ export default function Index() {
     <div className="flex h-screen items-center justify-center">
       <div className="flex flex-col items-center gap-16">
         <header className="flex flex-col items-center gap-9">
+          <div className="text-gray-700 dark:text-gray-200">
+            chore: ci/cd testing
+          </div>
           <h1 className="leading text-2xl font-bold text-gray-800 dark:text-gray-100">
             Welcome to <span className="sr-only">Remix</span>
           </h1>
